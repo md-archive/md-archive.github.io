66c4d5ba75c331dae2c0e48a54adce53220f2c5b
md-archive <dev@md-archive.com>
2022-11-29 18:20:46 +0100

    ok

--- DJANGO_FRAMEWORK/md_archive_com/login/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('<int:id>', views.index, name="index"),
-    path("home", views.home, name="home"),
-    path("create", views.create, name="create"),
-    ]
